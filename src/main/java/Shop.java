3adaadbc8693c3413f9d0d019fde09ff36ea0979
user <user@users-MBP-107.lan>
2018-11-25 17:24:17 +0000

    extention calculate total markup

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class Shop {
 
     private String name;
+//    private double profitMargin;
     private ArrayList<ISell> stock;
 
 
     public Shop(String name){
         this.stock = new ArrayList<ISell>();
         this.name = name;
+//        this.profitMargin = 0.0;
     }
 
     public String getName() {
@@ -28,6 +30,29 @@ public class Shop {
     public void removeFromStock(ISell item){
         this.stock.remove(item);
     }
+
+    public double calculateMarkup(ISell item){
+        double profitMargin = 0;
+        for (ISell iSell : stock) {
+            profitMargin += iSell.calculateMarkup();
+        }
+        return profitMargin;
+    }
+
+
+
+//    public double calculateTotalMarkup(ISell item){
+//
+//        double total = 0;
+//        for(ISell iSell : stock ) {
+//            iSell.calculateMarkup();
+//            total ++;
+//        }
+//        return total;
+//
+//    }
+
+
 
 
 
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,3 +1,4 @@
+import accessories.GuitarStrings;
 import behaviours.ISell;
 import instruments.Guitar;
 import org.junit.Before;
@@ -9,11 +10,13 @@ public class ShopTest {
 
     Shop shop;
     Guitar guitar;
+    GuitarStrings guitarStrings;
 
     @Before
     public void setUp() throws Exception {
         shop = new Shop("Ray's Music Exchange");
         guitar = new Guitar("Mahogany", "Archtop", "New", 200, 300, "Gibson", "ES 175", "Sunburst");
+        guitarStrings = new GuitarStrings("Gut", "Apple", "Nuts", 3, 6);
     }
 
     @Test
@@ -32,5 +35,15 @@ public class ShopTest {
         shop.addToStock(guitar);
         shop.removeFromStock(guitar);
         assertEquals(0, shop.itemCount());
+    }
+
+    @Test
+    public void canCalculateMarkUp() {
+        shop.addToStock(guitar);
+        shop.addToStock(guitar);
+        shop.addToStock(guitarStrings);
+        assertEquals(203, shop.calculateMarkup(guitar), 0.01);
+
+
     }
 }
